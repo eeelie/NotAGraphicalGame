07a7a36361539c0c8cf095c68fef2a14127549d8
hmburt <hmburt@princeton.edu>
2022-12-13 21:39:27 -0500

    add test for update_balls_left method

--- tests/test_player.py
+++ tests/test_player.py
@@ -23,10 +23,35 @@ def test_assign_team():
     assert player1.team == "stripes"
     assert player2.team == "solids"
 
+    assert player1.balls_left == [9,10,11,12,13,14,15]
+    assert player2.balls_left == [1,2,3,4,5,6,7]
+
 def test_assign_bad_team_name():
     with raises(Exception):
         bad_player = ("Hayden")
         bad_player.assign_team("polka-dots")
 
 def test_update_balls_left():
-    ...    
+    pocketed = [1,5,12]
+    player1 = Player("Jose")
+    player2 = Player("Elie")
+    player3 = Player("Jack")
+    player1.assign_team("solids")   
+    player2.assign_team("stripes")  
+
+    assert player1.balls_left == [1,2,3,4,5,6,7]
+    assert player2.balls_left == [9,10,11,12,13,14,15]
+    assert player3.balls_left == []
+
+    player1.update_balls_left(pocketed)
+    player2.update_balls_left(pocketed)
+    player3.update_balls_left(pocketed)
+
+    assert player1.balls_left == [2,3,4,6,7]
+    assert player2.balls_left == [9,10,11,13,14,15]
+    assert player3.balls_left == []
+
+    assert player1.down_to_the_eight == False
+    assert player2.down_to_the_eight == False
+    
+
