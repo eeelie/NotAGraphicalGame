31f5d4a286a30aab2588d6820ee014c47a63e4da
eeelie <51966008+eeelie@users.noreply.github.com>
2022-12-14 22:34:20 -0500

    Fix: fixed stray # missing from comment

--- src/LetsPlayPool.py
+++ src/LetsPlayPool.py
@@ -1,4 +1,3 @@
-
 from __future__ import annotations
 from game import *
 from visualize import *
@@ -7,30 +6,29 @@ import datetime
 player1_name = input("Player 1, what's your name?")
 player2_name = input("Player 2, what's your name?")
 
-game = Game(player1_name,player2_name)
+game = Game(player1_name, player2_name)
 game.start_game()
 
 print(f"{player1_name}, your team is {game.players[0].team}")
 print(f"{player2_name}, your team is {game.players[1].team}")
 
 game_over = False
-#every run of the loop will be one turn of the game
-while not game_over:        
-       
-    #take input for that player
+# every run of the loop will be one turn of the game
+while not game_over:
+
+    # take input for that player
     print(f"{game.current_player_name()}, your turn:")
     velocity, angle = take_input()
 
-    #update the state
+    # update the state
     game.update_state(velocity, angle)
 
     pocketed = game.pocketed_this_turn()
-    
 
     # update each player's balls left list
     game.update_players()
-    
-    output graph
+
+    # output graph
     graph_state(game.running_state)
     print(f"number of states to be graphed: {len(game.running_state.log)}")
     anim = animate(game.running_state.log)
@@ -42,7 +40,7 @@ while not game_over:
     open_visualization(file_name)
 
     print(f"The balls pocketed this turn were {*pocketed,}")
-    
+
     # end of game logic
     if 8 in pocketed:
         print(f"Game over! {game.winner()} wins!")
