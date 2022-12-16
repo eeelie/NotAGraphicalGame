cea9bed59833e761de673a18c69daa886e440435
hmburt <hmburt@princeton.edu>
2022-12-15 19:51:12 -0500

    clean code using precommit

--- tests/test_visualize.py
+++ tests/test_visualize.py
@@ -3,15 +3,18 @@ from visualize import graph_state
 import pytest
 from pytest import raises
 
+
 def start_game_graph():
     game = Game("A", "B")
     game.start_game()
     return game
 
+
 @pytest.mark.mpl_image_compare
 def test_initial_graph():
     fig = graph_state(start_game_graph().running_state)
     return fig
+
 
 @pytest.mark.mpl_image_compare
 def test_update_changes_graph():
