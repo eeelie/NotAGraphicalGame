06321d803390676c31a6aa8ab1883f5bf3ce08a5
Jose <jayala@princeton.edu>
2022-12-16 20:57:58 -0500

    changing how visuals are saved

--- src/LetsPlayPool.py
+++ src/LetsPlayPool.py
@@ -1,45 +1,59 @@
+
 from __future__ import annotations
+
+import matplotlib.pyplot as plt
+
 from game import *
 from visualize import *
-import time
+import datetime
 
-player1_name = input("Player 1, what's your name? ")
-player2_name = input("Player 2, what's your name? ")
+player1_name = input("Player 1, what's your name?")
+player2_name = input("Player 2, what's your name?")
 
-game = Game(player1_name, player2_name)
-game.start_game(time.time())
-open_visualization("game_start.jpg")
+game = Game(player1_name,player2_name)
+game.start_game()
+start_fig = graph_state(game.running_state)
+plt.savefig("start_fig.jpg")
+open_visualization("start_fig.jpg")
 
 print(f"{player1_name}, your team is {game.players[0].team}")
 print(f"{player2_name}, your team is {game.players[1].team}")
 
 game_over = False
-# every run of the loop will be one turn of the game
-while not game_over:
-
-    # take input for that player
+#every run of the loop will be one turn of the game
+while not game_over:        
+       
+    #take input for that player
     print(f"{game.current_player_name()}, your turn:")
     velocity, angle = take_input()
 
-    # update the state
+    #update the state
     game.update_state(velocity, angle)
 
     pocketed = game.pocketed_this_turn()
 
     # update each player's balls left list
     game.update_players()
-
-    # output graph
+    
+    #output graph
     anim = animate(game.running_state.log)
-    file_name = "replay.mp4"
+    time = datetime.datetime.now()
+    file = f"{time.month}-{time.day}--{time.hour}-{time.minute}-{time.second}--graphic"
     video_writer = animation.FFMpegWriter(fps=24)
     print("Game is being animated")
-    anim.save(file_name, writer=video_writer)
+    anim.save(file + ".mp4", writer=video_writer)
 
-    open_visualization(file_name)
+    last_state = graph_state(game.running_state)
+
+    plt.savefig(file +".jpg")
+    open_visualization(file + ".jpg")
+
+    open_visualization(file + ".mp4")
+
+
 
     print(f"The balls pocketed this turn were {*pocketed,}")
-
+    
     # end of game logic
     if 8 in pocketed:
         print(f"Game over! {game.winner()} wins!")
