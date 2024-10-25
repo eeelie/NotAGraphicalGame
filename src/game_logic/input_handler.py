e7432db38d9e471671991f46d2c9d813311ff897
eeelie <elie.svoll@gmail.com>
2024-10-25 14:17:59 -0700

    retry velocity/angle input until it's valid

--- src/game_logic/input_handler.py
+++ src/game_logic/input_handler.py
@@ -1,15 +1,31 @@
-from src.player.player import PlayerID
+from src.game_logic.player import PlayerID
 
 class InputHandler:
 
     @classmethod
     def get_name(cls, player_id:PlayerID) -> str:
-        return input(f"Player {player_id.value}, what is your name?")
+        return input(f"Player {player_id.value}, what is your name? ")
+
+    @staticmethod
+    def convert_to_float_infinite_retries(func:callable) -> callable:
+         
+        def wrapper(*args, **kwargs) -> float:
+            while True:
+                try:
+                    str_input = func(*args, **kwargs)
+                    float_input = float(str_input) # this will break if the input is invalid
+                    return float_input
+                except ValueError:
+                    print("\nInput must be a valid number!\n")
+        
+        return wrapper
 
     @classmethod
-    def get_velocity(cls) -> float:
-        return float(input("Input a velocity in m/s"))
+    @convert_to_float_infinite_retries
+    def get_velocity(cls) -> str:
+        return input("Input a velocity in m/s ")
         
     @classmethod
-    def get_angle(cls) -> float:
-        return float(input("Input an angle in degrees"))
+    @convert_to_float_infinite_retries
+    def get_angle(cls) -> str:
+        return input("Input an angle in degrees ")
